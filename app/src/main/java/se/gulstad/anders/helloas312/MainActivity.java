7ba3d5713e0f49959cb83164ad78adc4a46a0e43
Anders Gulstad <anders_gulstad@msn.com>
2018-05-05 12:57:39 +0200

    Next update

--- app/src/main/java/se/gulstad/anders/helloas312/MainActivity.java
+++ app/src/main/java/se/gulstad/anders/helloas312/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //ToDo Add more code...
+
+    // New code
+
 }
