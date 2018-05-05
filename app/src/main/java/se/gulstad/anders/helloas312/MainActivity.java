f8836e1ba02fe746ac0c283b5221e992ac3d131e
Anders Gulstad <anders_gulstad@msn.com>
2018-05-05 10:30:24 +0200

    Starting

---
+++ app/src/main/java/se/gulstad/anders/helloas312/MainActivity.java
@@ -0,0 +1,13 @@
+package se.gulstad.anders.helloas312;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
